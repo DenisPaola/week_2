b36f0b9c1508a07a5bd19617b434d55b2c8448c6
denispaola <denislozanoc@gmail.com>
2023-05-13 16:52:38 +0200

    reto 1 y 2

---
+++ typescript/person.ts
@@ -0,0 +1,27 @@
+export class Person {
+    public name: string
+    public age: number
+    private address: string
+
+    constructor(name, age, address) {
+        this.name = name
+        this.age = age
+        this.address = address
+    }
+
+    public printName(): void {
+        console.log(this.name)
+    }
+
+    public yearOfBirth(currentYear:number): number {
+        return currentYear - this.age
+    }
+
+    public setAddress(address:string): void {
+        this.address = address
+    }
+
+    public getAddress():string {
+        return this.address
+    }
+}
